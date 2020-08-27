7ea418635e8042ff738df5313918a738a0b7cbb4
Saad Mehmood <saadmehmood758@gmail.com>
2020-08-27 22:52:50 +0500

    mail

--- header.php
+++ header.php
@@ -1,3 +1,16 @@
+
+<?php
+
+$to = 'saadmehmood758@gmail.com';
+$subject = 'the subject';
+$message = 'hello';
+$headers = 'From: webmaster@example.com' . "\r\n" .
+    'Reply-To: webmaster@example.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+mail($to, $subject, $message, $headers);
+
+?>
 <!DOCTYPE html>
 
 <html lang="en">
