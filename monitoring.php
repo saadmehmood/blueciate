b09f9ebc193b6f361a4d7d7a4c0aa99c282ed5bd
Saad Mehmood <saadmehmood758@gmail.com>
2020-12-28 20:55:09 +0500

    monitoring

---
+++ monitoring.php
@@ -0,0 +1,198 @@
+﻿<?php
+$title = 'Consulting :- Blueciate Inc';
+include('head.php');
+?>
+<body>
+
+<div class="mm-page mm-slideout">
+    <div id="wrapper">
+        <?php
+        require_once 'header-2.php';
+        $images = ['bpm-7.png'];
+        include('image-slider.php');
+        ?>
+
+        <article id="services" class="hh-services">
+            <div class="container">
+                <section>
+                    <aside class="g lg-twelfths md-one-whole" id="first">
+                        <h3>The monitoring solution for all areas of IT</h3>
+                        <p style="max-width: 1200px;">
+                            IT has many different areas, each with their own monitoring requirements: network, infrastructure, hardware and applications are just a few examples.
+                            <br>
+                            BlueCiate Implementation Partnering  with PRTG has everything you need to monitor all areas of your IT.
+                        </p>
+                        <p style="max-width: 1200px;">
+                            Unlimited version of PRTG for 30 days. After 30 days, PRTG reverts to a free version.
+                            Or, you can upgrade to a paid license anytime.
+
+                        </p>
+                    </aside>
+                </section>
+                <section class="row mx-0 ">
+                    <div class="col-md-6 px-0 py-xl-5">
+                        <div class="fa-2x mx-auto" style="width: 275px;">Consultancy Team</div>
+                        <div class="fa-1x mx-auto" style="width: 80%;">Your BPM Success by putting your first project in our hands.</div>
+                        <div class="fa-1x mx-auto mt-4" style="width: 22%;">
+                            <a href="https://www.auraquantic.com/bpm-digital-platform/process-automation" class="buttonmore">Auraquantic</a>
+                        </div>
+                    </div>
+                    <div class="col-md-6 px-0">
+                        <img src="/img/rm-1.jpg" height="513px">
+                    </div>
+
+                </section>
+            </div>
+        </article>
+
+<br>
+        <!--BLOCK SECTION-->
+        <section class="six_box_sec services-blocks-sm" id="six_box_sec">
+            <div class="">
+                <div class="row no-gutters">
+                    <div class="col-12 col-lg-3 col-sm-6 pr-1 pl-1 services-block-four">
+                        <div class="inner-box wow fadeInLeft animated" data-wow-delay="0ms" data-wow-duration="1500ms"
+                             style="visibility: visible;animation-duration: 1500ms;animation-delay: 0ms;/* animation-name: fadeInLeft; */">
+                            <div class="image">
+                                <a href="https://www.paessler.com/firewall_monitoring"><img src="./img/montoring.png" alt=""></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-lg-3 col-sm-6 pr-1 pl-1 services-block-four">
+                        <div class="inner-box wow fadeInLeft animated" data-wow-delay="0ms" data-wow-duration="1500ms"
+                             style="visibility: visible;animation-duration: 1500ms;animation-delay: 0ms;/* animation-name: fadeInLeft; */">
+                            <div class="image">
+                                <a href="https://www.paessler.com/ip_monitoring"><img src="./img/ip-monitoring.png" alt=""></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-lg-3 col-sm-6 pr-1 pl-1 services-block-four">
+                        <div class="inner-box wow fadeInLeft animated" data-wow-delay="0ms" data-wow-duration="1500ms"
+                             style="visibility: visible;animation-duration: 1500ms;animation-delay: 0ms;/* animation-name: fadeInLeft; */">
+                            <div class="image">
+                                <a href="https://www.paessler.com/lan_monitor"><img src="./img/lan-monitoring.png" alt=""></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-lg-3 col-sm-6 pr-1 pl-1 services-block-four">
+                        <div class="inner-box wow fadeInLeft animated" data-wow-delay="0ms" data-wow-duration="1500ms"
+                             style="visibility: visible;animation-duration: 1500ms;animation-delay: 0ms;/* animation-name: fadeInLeft; */">
+                            <div class="image">
+                                <a href="https://www.paessler.com/wifi-monitoring"><img src="./img/wifi-monitoring.png" alt=""></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row no-gutters mt-2">
+                    <div class="col-12 col-lg-3 col-sm-6 pr-1 pl-1 services-block-four">
+                        <div class="inner-box wow fadeInLeft animated" data-wow-delay="0ms" data-wow-duration="1500ms"
+                             style="visibility: visible;animation-duration: 1500ms;animation-delay: 0ms;/* animation-name: fadeInLeft; */">
+                            <div class="image">
+                                <a href="https://www.paessler.com/network-monitoring-software"><img src="./img/network-diagnosis.png" alt=""></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-lg-3 col-sm-6 pr-1 pl-1 services-block-four">
+                        <div class="inner-box wow fadeInLeft animated" data-wow-delay="0ms" data-wow-duration="1500ms"
+                             style="visibility: visible;animation-duration: 1500ms;animation-delay: 0ms;/* animation-name: fadeInLeft; */">
+                            <div class="image">
+                                <a href="https://www.paessler.com/network-optimization"><img src="./img/network-monitoring.png" alt=""></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-lg-3 col-sm-6 pr-1 pl-1 services-block-four">
+                        <div class="inner-box wow fadeInLeft animated" data-wow-delay="0ms" data-wow-duration="1500ms"
+                             style="visibility: visible;animation-duration: 1500ms;animation-delay: 0ms;/* animation-name: fadeInLeft; */">
+                            <div class="image">
+                                <a href="https://www.paessler.com/network-security-monitoring"><img src="./img/network.png" alt=""></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-lg-3 col-sm-6 pr-1 pl-1 services-block-four">
+                        <div class="inner-box wow fadeInLeft animated" data-wow-delay="0ms" data-wow-duration="1500ms"
+                             style="visibility: visible;animation-duration: 1500ms;animation-delay: 0ms;/* animation-name: fadeInLeft; */">
+                            <div class="image">
+                                <a href="https://www.paessler.com/network_latency"><img src="./img/latency-monitoring.png" alt=""></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--BLOCK SECTION-->
+
+        <br/>
+        <article id="contact--details" class="block">
+            <div class="container">
+                <section>
+                    <div class="gw" style="font-size:15px;">
+                        <div class="g lg-three-twelfths">
+<!--                            <h4>Contact Us</h4>-->
+                            <h1>Get in touch today</h1>
+                            <p>
+                                BlueCiate Team is always here to listen you.
+                            </p>
+                        </div>
+                        <div class="g lg-one-twelfth">&nbsp;</div>
+                        <div class="g lg-two-twelfths">
+                            <aside>
+                                <span class="icon-phone"></span>
+                                <small><?php echo $phone; ?> </small>
+
+                            </aside>
+                        </div>
+                        <div class="g lg-one-twelfth">&nbsp;</div>
+                        <div class="g lg-two-twelfths">
+                            <aside>
+                                <span class="icon-envelope"></span>
+                                <small><a href="mailto:info@blueciate.com?subject=feedback">info@blueciate.com</a></small>
+                            </aside>
+                        </div>
+                        <div class="g lg-one-twelfth">&nbsp;</div>
+                        <div class="g lg-two-twelfths">
+                            <aside>
+                                <span class="icon-map-pin"></span>
+                                <small><?php echo $address; ?></small>
+                                <small><?php echo $addressLine2; ?></small>
+                            </aside>
+                        </div>
+                    </div>
+
+                </section>
+
+            </div>
+        </article>
+        <?php
+        include('footer.php');
+        ?>
+    </div>
+</div>
+
+<!-- jQuery -->
+
+<script src="../js/jquery.js"></script>
+<script src="../js/slick.min.js"></script>
+<script src="../js/share.min.js"></script>
+<script src="../js/classie.js"></script>
+<script src="../js/elements.js"></script>
+<script src="../js/jquery.animate-enhanced.min.js"></script>
+<script src="../js/jquery.superslides.min.js"></script>
+<script src="../js/jquery.easing.js"></script>
+<script src="../js/hammer.min.js"></script>
+<script src="../js/scriptmap.js"></script>
+<script src="../js/gmap.js"></script>
+<script src="../js/validate.min.js"></script>
+<script src="../js/jquery.mmenu.min.all.js"></script>
+<script src="../js/theater.min.js"></script>
+<script src="../js/jquery.superslides.min.js"></script>
+<script src="../js/jquery.flexslider-min.js"></script>
+<script src="../js/imagelightbox.min.js"></script>
+
+<!-- use this url for more information about share button plugin -->
+<!-- https://github.com/carrot/share-button/wiki/Configuration-Options -->
+<script src="../js/scripts.js"></script>
+
+</body>
+
+
+</html>
