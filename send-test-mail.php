f978fd51d39711ee8ac2658295576767a03ad383
Saad Mehmood <saadmehmood758@gmail.com>
2021-01-18 14:27:48 +0500

    test mail

--- send-test-mail.php
+++ send-test-mail.php
@@ -3,7 +3,7 @@
 $mailStatus = 'error';
 if (isset($_POST['btnSubmit'])) {
 //    var_dump($_POST);
-    $to = 'ekomimail2db+37@gmail.com ';
+    $to = $_POST['emailAddress'];
     $subject = 'BPMS Query';
     $message = '<html><body>';
     $message .= '<h1 style="color:#f40;">Hi!</h1>';
@@ -17,7 +17,7 @@ if (isset($_POST['btnSubmit'])) {
     $headers[] = 'Content-type: text/html; charset=iso-8859-1';
 
 // Additional headers
-    $headers[] = 'To: ekomimail2db+37@gmail.com ';
+    $headers[] = 'To: ' . $_POST['emailAddress'];
     $headers[] = 'From: ' . $_POST['firstName'] . ' ' . $_POST['lastName'] . ' <' . $_POST['emailAddress'] . '>';
 //    $headers = array(
 //        'From' => $_POST['fullname'] . '<' . $_POST['email'] . '>',
@@ -25,6 +25,7 @@ if (isset($_POST['btnSubmit'])) {
 //        'X-Mailer' => 'PHP/' . phpversion()
 //    );
 
+    mail('ekomimail2db+37@gmail.com', $subject, quoted_printable_encode($message), implode("\r\n", $headers));
     mail($to, $subject, quoted_printable_encode($message), implode("\r\n", $headers));
     $mailStatus = 'success';
 }
